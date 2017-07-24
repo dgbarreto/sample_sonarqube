bce8987ff3c13710b5d55418039f90739ab455a0
Danilo Barreto <Danilo Barreto>
2017-07-24 14:28:08 -0300

    Adicionar arquivos de projeto.

---
+++ SampleSonaQube/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSonaQube {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Sample");
+            if (1 == 2)
+                Console.WriteLine("Impossible");
+
+            Console.ReadKey();
+        }
+    }
+}
